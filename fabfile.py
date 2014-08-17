c58185de2d881e2b2aae1cfabed22b5f00c35225
tan yong <violin748@hotmail.com>
2014-08-17 06:00:05 +0000

    update

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,7 @@ from fabric.api import local
 
 def deploy():
 	local('git add .')
-	comment=raw_input()
+	comment=raw_input('please key in some words:')
 	local('git commit -m "%s"' % comment)
 	local('git push origin master')
 
